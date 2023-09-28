b65b23de2f49e50509064e0764d06250866bfe76
OlgaOrtiz <ortizquintana.olga@gmail.com>
2023-09-28 12:33:46 +0200

    - Added temporal icons to the buttons - Added the click to change feature to every button

--- assets/js/general.js
+++ assets/js/general.js
@@ -1,7 +1,25 @@
 const weigth = document.querySelector("#weigth");
 weigth.addEventListener("click", openWeigth);
+const length = document.querySelector("#length");
+length.addEventListener("click", openLength);
+const coin = document.querySelector("#coin");
+coin.addEventListener("click", openCoin);
+const temperature = document.querySelector("#temperature");
+temperature.addEventListener("click", openTemperature);
+
 
 function openWeigth (){
-    console.log("abriendo weigth");
+    window.location.href = "http://localhost:8000/converter-generic.html";
+}
+
+function openLength (){
+    window.location.href = "http://localhost:8000/converter-generic.html";
+}
+
+function openCoin (){
+    window.location.href = "http://localhost:8000/converter-generic.html";
+}
+
+function openTemperature (){
     window.location.href = "http://localhost:8000/converter-generic.html";
 }

9b400899cc0ca0bee52570642b58a918ec4cb11e
OlgaOrtiz <ortizquintana.olga@gmail.com>
2023-12-26 14:43:10 +0100

    - Added toFixed(3) to truncate the results to three decimals

--- assets/js/currencyConverter.js
+++ assets/js/currencyConverter.js
@@ -1,23 +1,23 @@
 export function euroToDollar (inputValue, rate) {
-    return parseFloat(inputValue.replace(",",".")) * rate;
+    return (parseFloat(inputValue.replace(",",".")) * rate).toFixed(3);
 }
 
 export function euroToPound(inputValue, rate) {
-    return parseFloat(inputValue.replace(",",".")) * rate;
+    return (parseFloat(inputValue.replace(",",".")) * rate).toFixed(3);
 }
 
 export function dollarToEuro(inputValue, rate) {
-    return parseFloat(inputValue.replace(",",".")) * rate;
+    return (parseFloat(inputValue.replace(",",".")) * rate).toFixed(3);
 }
 
 export function dollarToPound(inputValue, rate) {
-    return parseFloat(inputValue.replace(",",".")) * rate;
+    return (parseFloat(inputValue.replace(",",".")) * rate).toFixed(3);
 }
 
 export function poundToEuro(inputValue, rate) {
-    return parseFloat(inputValue.replace(",",".")) * rate;
+    return (parseFloat(inputValue.replace(",",".")) * rate).toFixed(3);
 }
 
 export function poundToDollar(inputValue, rate) {
-    return parseFloat(inputValue.replace(",",".")) * rate;
+    return (parseFloat(inputValue.replace(",",".")) * rate).toFixed(3);
 }
--- assets/js/weightConverter.js
+++ assets/js/weightConverter.js
@@ -1,23 +1,23 @@
 export function lbToKilo (inputValue) {
-    return parseFloat(inputValue.replace(",",".")) * 0.4535;
+    return (parseFloat(inputValue.replace(",",".")) * 0.4535).toFixed(3);
 }
 
 export function lbToTon  (inputValue) {
-    return parseFloat(inputValue.replace(",", ".")) * 0.0004;
+    return (parseFloat(inputValue.replace(",", ".")) * 0.0004).toFixed(3);
 }
 
 export function kgToPound (inputValue) {
-    return parseFloat(inputValue.replace(",", ".")) * 2.2046;
+    return (parseFloat(inputValue.replace(",", ".")) * 2.2046).toFixed(3);
 }
 
 export function kgToTon (inputValue) {
-    return parseFloat(inputValue.replace(",", ".")) / 1000;
+    return (parseFloat(inputValue.replace(",", ".")) / 1000).toFixed(3);
 }
 
 export function tToKilo (inputValue) {
-    return parseFloat(inputValue.replace(",", ".")) * 1000;
+    return (parseFloat(inputValue.replace(",", ".")) * 1000).toFixed(3);
 }
 
 export function tToPound (inputValue) {
-    return parseFloat(inputValue.replace(",", ".")) * 2204.6226;
+    return (parseFloat(inputValue.replace(",", ".")) * 2204.6226).toFixed(3);
 }
